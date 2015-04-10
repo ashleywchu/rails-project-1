9908f804395aa1b37fe28b35f35507c1b740eea8
github username <ashleywchu@gmail.com>
2015-04-10 15:26:10 -0400

    added validations for city

--- app/models/city.rb
+++ app/models/city.rb
@@ -1,8 +1,10 @@
 class City < ActiveRecord::Base
 	has_many :users
 	has_many :companies
+
+	validates :name, uniqueness: true, presence: true
 	
-   def self.create(location)
+  def self.create(location)
     city = City.new
     city.name = location
     city.save
