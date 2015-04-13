36dcf6dff4375e088dcaae50d0c4a4814a322a8f
github username <ashleywchu@gmail.com>
2015-04-12 20:54:52 -0400

    fixing post model validation error

--- app/models/post.rb
+++ app/models/post.rb
@@ -4,6 +4,14 @@ class Post < ActiveRecord::Base
 
 	validates_uniqueness_of :user_id, :scope => [:name]
 	
+	before_validation :ensure_fields_filled
 	validates :name, presence: true
 	validates :content, presence: true
+
+	private
+	def ensure_fields_filled
+		if name.empty? && content.empty?
+
+		end
+	end
 end
